0ce130bf230ca9f74b876c6b245fdc5e1add0016
csrigogogo <cri.sri@outlook.com>
2022-04-15 15:17:45 +0800

    add test

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2022-04-15 14:36:28
- * @LastEditTime: 2022-04-15 14:58:27
+ * @LastEditTime: 2022-04-15 15:16:09
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \minigrep\src\lib.rs
  */
 use std::error::Error;
 use std::fs;
-
 
 pub struct Config {
     pub query: String,
@@ -28,8 +27,36 @@ impl Config {
 }
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+    // println!("With text:\n{}", contents);
 
     Ok(())
+}
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            // 对文本行进行操作
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
